9793564e731e02bf74be9be961c1afb48a5bbcc0
EmmaZhang <emmazxd@gmail.com>
2024-03-13 14:48:55 +1100

    light up state to app from Nav

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import { useState } from "react";
 
 function App() {
   const [theme, setTheme] = useState("light-mode");
+  const [clicked, setClicked] = useState(true);
+  const buttonText = clicked ? "Light Mode" : "Dark Mode";
+
+  function handleModeChange() {
+    setClicked(!clicked);
+    setTheme(clicked ? "dark-mode" : "light-mode");
+  }
 
   return (
     <div>
-      <Header />
+      <Header handleModeChange={handleModeChange} buttonText={buttonText} />
       <Main theme={theme} />
       <Footer theme={theme} />
     </div>
